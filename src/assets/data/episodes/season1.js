10266ab517d2eb130f725b4a93d395e4fb0dce03
veljko-03 <vspasic03@gmail.com>
2026-01-08 19:29:03 +0100

    season 1

---
+++ src/assets/data/episodes/season1.js
@@ -0,0 +1,50 @@
+export const season1 = [
+  {
+    id: 1,
+    title: "Chapter One: The Vanishing of Will Byers",
+    description: "On his way home from a friend's house, Will sees something terrifying. Nearby, a sinister secret lurks in the depths of a government lab.",
+    thumbnail: "",
+  },
+  {
+    id: 2,
+    title: "Chapter Two: The Weirdo on Maple Street",
+    description: "Lucas, Mike and Dustin try to talk to the girl they found in the woods. Hopper questions an anxious Joyce about an unsettling phone call.",
+    thumbnail: "",
+  },
+  {
+    id: 3,
+    title: "Chapter Three: Holly, Jolly",
+    description: "Increasingly concerned, Nancy searches for Barb and tries to find out what Jonathan has been up to; Joyce thinks Will is trying to talk to her.",
+    thumbnail: "",
+  },
+  {
+    id: 4,
+    title: "Chapter Four: The Body",
+    description: "Refusing to accept that Will is dead, Joyce tries to connect with him; the boys decide to give Eleven a makeover; Jonathan and Nancy form an unexpected alliance.",
+    thumbnail: "",
+  },
+  {
+    id: 5,
+    title: "Chapter Five: The Flea and the Acrobat",
+    description: "Hopper gets into the lab; Jonathan and Nancy confront the force that took Will; the boys talk to Mr. Clarke about travelling to another dimension.",
+    thumbnail: "",
+  },
+  {
+    id: 6,
+    title: "Chapter Six: The Monster",
+    description: "A frantic Jonathan looks for Nancy in the darkness, but Steve's looking for her, too. Hopper and Joyce uncover the truth about the lab's experiments.",
+    thumbnail: "",
+  },
+  {
+    id: 7,
+    title: "Chapter Seven: The Bathtub",
+    description: "Eleven tries to reach Will; Lucas issues a warning about the arrival of the bad men; Jonathan and Nancy show the police what was caught on camera.",
+    thumbnail: "",
+  },
+  {
+    id: 8,
+    title: "Chapter Eight: The Upside Down",
+    description: "Dr Brenner holds Hopper and Joyce for questioning while the boys wait with Eleven in the gym. Back at Will's, Nancy and Jonathan prepare for battle.",
+    thumbnail: "",
+  },
+]
