a8bf0c0a83a9b10a612e62d10485eb58b8c86237
veljko-03 <vspasic03@gmail.com>
2026-01-08 22:27:54 +0100

    season 4

---
+++ src/assets/data/episodes/season4.js
@@ -0,0 +1,56 @@
+export const season4 = [
+  {
+    id: 1,
+    title: "Chapter One: The Hellfire Club",
+    description: "El is having trouble with bullying at school, Joyce opens a mysterious package which she is determined to decipher and a scrappy new player shakes up D&D night at the Hellfire Club.",
+    thumbnail: "/images/thumbnails/s4/s4e1.webp",
+  },
+  {
+    id: 2,
+    title: "Chapter Two: Vecna's Curse",
+    description: "A plane brings Mike to California and a dead body brings Hawkins to a halt. Nancy goes looking for leads and a shaken Eddie tells the gang what he saw.",
+    thumbnail: "/images/thumbnails/s4/s4e2.jpg",
+  },
+  {
+    id: 3,
+    title: "Chapter Three: The Monster and the Superhero",
+    description: "Murray and Joyce fly to Alaska, and El faces serious consequences. Meanwhile, Robin and Nancy dig up dirt on Hawkins' demons and Dr. Owens delivers sobering news.",
+    thumbnail: "/images/thumbnails/s4/s4e3.jpg",
+  },
+  {
+    id: 4,
+    title: "Chapter Four: Dear Billy",
+    description: "Max is in grave danger and running out of time and a patient at Pennhurst asylum receives visitors for the firs time in years. Meanwhile, in Russia, Hopper is hard at work.",
+    thumbnail: "/images/thumbnails/s4/s4e4.jpg",
+  },
+  {
+    id: 5,
+    title: "Chapter Five: The Nina Project",
+    description: "Owen takes El to Nevada, where she's forced to confront her past. Meanwhile, the Hawkins kids comb through a crumbling house for clues and Vecna claims another teenage victim.",
+    thumbnail: "/images/thumbnails/s4/s4e5.jpg",
+  },
+  {
+    id: 6,
+    title: "Chapter Six: The Dive",
+    description: "Behind the Iron Curtain, a risky rescue mission gets underway. In America, the California crew seeks help from a hacker and Steve takes one for the team in Hawkins.",
+    thumbnail: "/images/thumbnails/s4/s4e6.webp",
+  },
+  {
+    id: 7,
+    title: "Chapter Seven: The Massacre at Hawkins Lab",
+    description: "As Hopper braces to battle a monster, Dustin dissects Vecna's motives and decodes a message from beyond. Meanwhile, in the underground facility, El finds strength in a distant memory.",
+    thumbnail: "/images/thumbnails/s4/s4e7.avif",
+  },
+  {
+    id: 8,
+    title: "Chapter Eight: Papa",
+    description: "Nancy has sobering visions, and El passes an important test. Meanwhile, back in Hawkins, the gang gathers supplies and prepares for battle.",
+    thumbnail: "/images/thumbnails/s4/s4e8.jpg",
+  },
+  {
+    id: 9,
+    title: "Chapter Nine: The Piggyback",
+    description: "With selfless hearts and a clash of metal, heroes fight from every corner of the battlefield to save Hawkins and the world itself.",
+    thumbnail: "/images/thumbnails/s4/s4e9.avif",
+  },
+]
--- src/pages/EpisodesPage.jsx
+++ src/pages/EpisodesPage.jsx
@@ -5,6 +5,7 @@ import SeasonCollapsableList from "../components/SeasonCollapsableList";
 import { season1 } from "../assets/data/episodes/season1";
 import { season2 } from "../assets/data/episodes/season2";
 import { season3 } from "../assets/data/episodes/season3";
+import { season4 } from "../assets/data/episodes/season4";
 
 const EpisodesPage = () => (
   <div className="episodes-page">
@@ -15,6 +16,7 @@ const EpisodesPage = () => (
       <SeasonCollapsableList title="Season 1" episodes={season1} />
       <SeasonCollapsableList title="Season 2" episodes={season2} />
       <SeasonCollapsableList title="Season 3" episodes={season3} />
+      <SeasonCollapsableList title="Season 4" episodes={season4} />
     </div>
   </div>
 );
