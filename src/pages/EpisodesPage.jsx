f2377b9a311cc58473d0736e6e26c2f51d596879
veljko-03 <vspasic03@gmail.com>
2026-01-08 19:33:49 +0100

    chore: episodes page creation + navigation

---
+++ src/pages/EpisodesPage.jsx
@@ -0,0 +1,12 @@
+import UpsideDown from "../components/UpsideDown";
+import Navbar from "../components/NavBar";
+import "../styles/EpisodesPage.css";
+
+const EpisodesPage = () => (
+  <div className="episodes-page">
+    <UpsideDown />
+    <Navbar />
+  </div>
+);
+
+export default EpisodesPage;
