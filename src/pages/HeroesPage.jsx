09eb838745c7e16616707b7b0a14ace5ea7f4dcb
veljko-03 <vspasic03@gmail.com>
2026-01-16 17:12:17 +0100

    chore: heroes page

--- src/pages/HeroesPage.jsx
+++ src/pages/HeroesPage.jsx
@@ -1,47 +1,26 @@
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination, Mousewheel } from "swiper/modules";
-import "swiper/css";
-import "swiper/css/pagination";
-
 import { heroes } from "../assets/data/heroes";
 import "../styles/HeroesPage.css";
 import Navbar from "../components/NavBar";
-
+import UpsideDown from "../components/UpsideDown";
 
 const HeroesPage = () => {
   return (
     <div className="heroes-page">
+      <UpsideDown />
       <Navbar />
-      <div className="container">
-        <Swiper
-          modules={[Pagination, Mousewheel]}
-          grabCursor
-          centeredSlides
-          speed={1000}
-          initialSlide={4}
-          mousewheel={{ thresholdDelta: 30 }}
-          pagination={{ clickable: true }}
-          onClick={(swiper) => swiper.slideTo(swiper.clickedIndex)}
-          breakpoints={{
-            0: {
-              slidesPerView: 1,
-              spaceBetween: 0,
-            },
-            768: {
-              slidesPerView: "auto",
-              spaceBetween: 10,
-            },
-          }}
-          className="swiper"
-        >
-          {heroes.map((hero, index) => (
-            <SwiperSlide key={index}>
-              <img src={hero.img} alt={hero.name} />
-              <p>{hero.name}</p>
-              <span>{hero.actor}</span>
-            </SwiperSlide>
-          ))}
-        </Swiper>
+
+      <h1 className="heroes-title">Heroes</h1>
+
+      <div className="heroes-grid">
+        {heroes.map((hero, index) => (
+          <div className="hero-card" key={index}>
+            <img src={hero.img} alt={hero.name} />
+            <div className="hero-info">
+              <h2>{hero.name}</h2>
+              <p>{hero.actor}</p>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
